a98d449fabd2bfaa1ffa722ecc26409559d96161
eden <eden.reader@gmail.com>
2025-02-16 19:01:21 -0800

    Moved and added features to Roster.h

--- Roster.h
+++ Roster.h
@@ -110,22 +110,27 @@ public:
     }
 
     // drop an object
-    void dropStudent(string idnum)
-    {
-        int index = findStudent(idnum);
-        if (index != -1)
-        {
-            roster.erase(roster.begin() + index);
-            // update txt file
-            updateFile(); 
+    void dropStudent(string idnum){
 
-            cout << "Student with ID " << idnum << " is removed.\n";
-        }
-        else
+        int index = findStudent(idnum);
+        if (index == -1)
         {
-            cout << "Student not found." << endl;
+            cout << "Student not found!" << endl;
+            return;
         }
+
+        // Shift elements left to overwrite the deleted student
+        for (size_t i = index; i < size - 1; i++)
+        {
+            roster[i] = roster[i + 1];
+        }
+
+        size--; // Reduce the number of students
+
+        updateFile(); // Save changes to file
+        cout << "Student with ID " << idnum << " is removed.\n";
     }
+
 
     // display object (find the object, then print it)
     void printStudent(string idnum)
@@ -142,6 +147,61 @@ public:
         {
             cout << "Student not found, sorry!" << endl;
         }
+    }
+    void insertionSort(){
+        //name
+        for(size_t i = 1; i < size ; i++ ){
+            Student key = roster[i]; //object
+            size_t j = i - 1; //index
+            while(j >= 0 && roster[j].getfirstName() > key.getfirstName()){
+                roster[j+1] = roster[j]; //overwrites the right element with the one to its left
+                j--;
+            }
+            //putting item in hole
+            roster[j+1] = key; 
+        }
+    }
+    void selectionSort(){
+        //id
+        for(size_t i = 0; i < size - 1 ; i++){
+            int currentMin = i;
+            
+            for(int j = i + 1; j < size; j++ ){
+                if (roster[j].getID() < roster[currentMin].getID()){
+                    //update min is smaller elem. is found
+                    currentMin = j;
+                }
+            }
+            //once you exit this loop, it means you have found the smallest elem.
+            //so insert in to correct pos.
+            swap(roster[i], roster[currentMin]); 
+        }
+    }
+    int partition(int low, int high){
+        string pivot = roster[high].getgrade(); //choosing last elem. as pivot
+        int i = low - 1; //i tracks the last swapped pos
+        
+        for (int j = low; j < high; j++){
+            if (roster[j].getgrade() <= pivot){//compares with pivot
+                i++; //moves the boundary of smaller elem.
+                swap(roster[i], roster[j]);
+            }
+        }   
+        swap(roster[i + 1], roster[high]); //puts pivot in correct pos
+        return i + 1; //returns pivot pos
+
+    }
+    void quickSort(int low, int high){
+        //grade
+        if (low < high){
+           int partitionIndex = partition(low, high);
+
+           quickSort(low, partitionIndex - 1);//sort left side
+           quickSort(partitionIndex + 1, high);//sort right side
+        }
+    }
+    void quickSortWrap(){//wrapper function to call in main
+        quickSort(0, size - 1);
     }
 };
 
